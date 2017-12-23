2daa1e3534eb4021072c6baea61d1b88279f45ed
aasumitro <aasumitro@merahputih.id>
2017-12-24 07:57:48 +0800

    24/12/2017 - 07:57

--- app/src/main/java/id/asmith/someapp/data/local/db/SQLHanddler.kt
+++ app/src/main/java/id/asmith/someapp/data/local/db/SQLHanddler.kt
@@ -4,8 +4,8 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
-import org.jetbrains.anko.db.*
 import id.asmith.someapp.util.AppConstants as utils
 
 
@@ -16,52 +16,65 @@ import id.asmith.someapp.util.AppConstants as utils
  * aasumitro@gmail.com
  */
 
-class SQLHandler(context: Context) : ManagedSQLiteOpenHelper(context,
-        utils.DB_NAME, null, utils.DB_VERSION) {
+class SQLHandler(context: Context?) : SQLiteOpenHelper(context, utils.DB_NAME,
+        null, utils.DB_VERSION) {
 
     override fun onCreate(db: SQLiteDatabase) {
-        // Here you create tables
-        db.createTable("Customer", true,
-                "id" to INTEGER + PRIMARY_KEY + UNIQUE,
-                "uid" to TEXT,
-                "username" to TEXT,
-                "name" to TEXT,
-                "email" to TEXT + UNIQUE,
-                "phone" to TEXT,
-                "token" to TEXT,
-                "isLoggedIn" to TEXT,
-                "created_at" to TEXT,
-                "updated_at" to TEXT)
-        Log.d("Debug", "Databases Created")
+        val createTable: String = "Create table ${utils.DB_TABLE} " +
+                "(" +
+                    "${utils.KEY_ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "${utils.KEY_UID} TEXT, " +
+                    "${utils.KEY_USERNAME} TEXT," +
+                    "${utils.KEY_NAME} TEXT," +
+                    "${utils.KEY_EMAIL} TEXT," +
+                    "${utils.KEY_PHONE} TEXT, " +
+                    "${utils.KEY_TOKEN} TEXT," +
+                    "${utils.KEY_LOGGED} TEXT," +
+                    "${utils.KEY_CREATED} TEXT," +
+                    "${utils.KEY_UPDATED} TEXT " +
+                ")"
 
+        // Here you create tables
+        db.execSQL(createTable)
+
+        Log.d("Debug", "Databases Created")
     }
+
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         // Drop old
-        db.dropTable(utils.DB_TABLE, true)
+        db.execSQL("DROP TABLE IF EXISTS " + utils.DB_TABLE)
         // Create new
         onCreate(db)
+
+        Log.e("Debug", "Table is Exist and will deleted")
 
     }
 
     fun addUserData(uid: String, username: String, name: String, email: String, phone: String,
-                 token: String, logged: String, created_at: String, updated_at: String) {
+                    token: String, logged: String, created_at: String, updated_at: String) {
 
+        deleteUserData()
         val db: SQLiteDatabase = this.writableDatabase
         val values = ContentValues()
-        values.put("uid", uid)
-        values.put("username", username)
-        values.put("name", name)
-        values.put("email", email)
-        values.put("phone", phone)
-        values.put("token", token)
-        values.put("isLoggedIn", logged)
-        values.put("created_at", created_at)
-        values.put("updated_at", updated_at)
+        values.put(utils.KEY_UID, uid)
+        values.put(utils.KEY_USERNAME, username)
+        values.put(utils.KEY_NAME, name)
+        values.put(utils.KEY_EMAIL, email)
+        values.put(utils.KEY_PHONE, phone)
+        values.put(utils.KEY_TOKEN, token)
+        values.put(utils.KEY_LOGGED, logged)
+        values.put(utils.KEY_CREATED, created_at)
+        values.put(utils.KEY_UPDATED, updated_at)
         db.insert(utils.DB_TABLE, null, values)
         db.close()
 
-        Log.d("Debug", "Success get user data")
+        Log.d("Debug", "Success add user data $uid, $username, $name, $email, $phone," +
+                "$token, $logged, $created_at, $updated_at")
 
+    }
+
+    fun loggedStatusOut(logged: String) {
+        
     }
 
     fun deleteUserData() {
@@ -76,19 +89,17 @@ class SQLHandler(context: Context) : ManagedSQLiteOpenHelper(context,
         val userData = HashMap<String, String>()
         val db: SQLiteDatabase = this.readableDatabase
 
-        //anko belum bisa rawQuery
-        //db.select(utils.DB_TABLE).whereSimple("(uid = ? )", uid)
-
         val  cursor: Cursor = db.rawQuery("SELECT * FROM ${utils.DB_TABLE}",
                 null )
         cursor.moveToFirst()
         if (cursor.moveToFirst()) {
             userData.put("uid", cursor.getString(1))
-            userData.put("username", cursor.getString(1))
-            userData.put("name", cursor.getString(1))
-            userData.put("email", cursor.getString(1))
-            userData.put("phone", cursor.getString(1))
-            userData.put("token", cursor.getString(1))
+            userData.put("username", cursor.getString(2))
+            userData.put("name", cursor.getString(3))
+            userData.put("email", cursor.getString(4))
+            userData.put("phone", cursor.getString(5))
+            userData.put("token", cursor.getString(6))
+            userData.put("logged", cursor.getString(7))
         }
         cursor.close()
         db.close()
